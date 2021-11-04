c659e2fb95412ac38a2ca3a557187ecdb1fc73da
ChenChihYuan <aesthetik0717@gmail.com>
2021-11-04 20:50:43 +0900

    before tutorial 10

--- tutorial9.js
+++ tutorial9.js
@@ -1,3 +1,5 @@
 var name = window.prompt("Waht's your name?"); // get the value from Users
+var age = window.prompt("How old are you?");
 
-document.write("Your name is: "+name);
+
+document.write("Your name is: "+name + ", you're "+ age);
