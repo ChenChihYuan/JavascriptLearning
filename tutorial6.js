85b57959db23efc93ad56ae88996466fd2ec459c
ChenChihYuan <aesthetik0717@gmail.com>
2021-11-04 16:17:20 +0900

    ask user input using window.prompt

---
+++ tutorial6.js
@@ -0,0 +1,13 @@
+var phrase = "To be or not to be </br>";
+var height = 180;
+var gpa = 4.3;
+var isGood = true; 
+
+
+document.write(phrase);
+document.write(phrase);
+phrase = "changed line</br>";
+
+document.write(phrase);
+document.write(phrase.toUpperCase().substring(0,3));
+document.write(phrase.toUpperCase().substring(0,3).includes('A'));
---
+++ tutorial9.js
@@ -0,0 +1,3 @@
+var name = window.prompt("Waht's your name?"); // get the value from Users
+
+document.write("Your name is: "+name);
