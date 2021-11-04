9f025afb147630ab627ac46b7ee20c6f1dcbd393
ChenChihYuan <aesthetik0717@gmail.com>
2021-11-04 21:23:19 +0900

    function in JS

---
+++ 12.js
@@ -0,0 +1,10 @@
+function SayHi(name){
+    document.write("<h1>Hello "+ name+"</h1>");
+}
+
+function addition(num1, num2){
+    return num1 + num2;
+}
+
+SayHi("Kevin");
+document.write(addition(3, 2));
--- tutorial10.js
+++ tutorial10.js
@@ -1,6 +1,7 @@
 var num1 = window.prompt("Enter a number:");
 var num2 = window.prompt("Enter a number:");
 
-
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
 
 document.write(num1+num2);  // if you do add these two number together they will be concated as strings 
