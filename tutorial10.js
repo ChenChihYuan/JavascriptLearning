0d6d54f22d48109914a8137709ac7a6b04b2dbfa
ChenChihYuan <aesthetik0717@gmail.com>
2021-11-04 20:55:41 +0900

    number from users should be parsed

---
+++ tutorial10.js
@@ -0,0 +1,6 @@
+var num1 = window.prompt("Enter a number:");
+var num2 = window.prompt("Enter a number:");
+
+
+
+document.write(num1+num2);  // if you do add these two number together they will be concated as strings 
