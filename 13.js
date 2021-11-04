69d78bd2343f186f08b4bd39cb50345837c865e3
ChenChihYuan <aesthetik0717@gmail.com>
2021-11-04 21:31:57 +0900

    Accessing HTML elements document.getElementById()

---
+++ 13.js
@@ -0,0 +1,9 @@
+
+var header = document.getElementById("header");
+header.innerHTML = "Overridden";
+header.style.color = "red";
+header.style.background = "blue";
+
+
+var link = document.getElementById("link");
+link.href = "https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML";
