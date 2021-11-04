28147d6cfd6df4ab72df6dcb051028eb096308ae
ChenChihYuan <aesthetik0717@gmail.com>
2021-11-04 21:20:54 +0900

    function in 12

---
+++ 11.js
@@ -0,0 +1,6 @@
+var fruits = new Array("Apples", "Oranges","Peaches");
+//Same as -> var fruits = ["Apples", "Oranges","Peaches"];
+fruits = new Array("Apples", 34/3 ,true);
+
+
+document.write(fruits);
